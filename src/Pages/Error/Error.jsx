845fdcb3c0253e1c26a381011da9d468bd0e6191
Kade <106041496+kaderathbun@users.noreply.github.com>
2022-11-20 23:00:03 -0500

    013.1 - padding on error page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -8,15 +8,15 @@ import { FaSpinner } from 'react-icons/fa'
 function Error() {
   const navigate = useNavigate()
 
-  setTimeout(() => {
-    navigate('/galleria')
-  }, 5000)
+  // setTimeout(() => {
+  //   navigate('/galleria')
+  // }, 5000)
 
   return (
     <div className="error">
       <section className="error__container">
         <h1>Sorry, this page does not exist.</h1>
-        <p>Please wait while we redirect you.</p>
+        <p>Please wait while we redirect you to the home page.</p>
         <FaSpinner className="error__loading" />
       </section>
     </div>
